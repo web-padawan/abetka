e9d6fc69558c3ee64ad3b64361d2bf168c3b25ed
web-padawan <skamandr@yandex.ru>
2015-12-27 16:22:43 +0200

    [mod] store notes in app state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,23 +2,30 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Profit'
-  }
-];
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Profit'
+        }
+      ]
+    };
+  }
   render() {
+    const notes = this.state.notes;
+
     return (
       <div>
         <Notes items={notes} />
