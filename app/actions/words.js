8bcdb5bd631e4327df9d987cf30ff394acc93aff
web-padawan <skamandr@yandex.ru>
2016-01-07 12:12:48 +0200

    [mod] move action types to separate file

--- app/actions/words.js
+++ app/actions/words.js
@@ -1,9 +1,10 @@
+import * as types from '../constants/ActionTypes';
 import uuid from 'node-uuid';
 
 export const ADD_WORD = 'ADD_WORD';
 export function addWord(word) {
   return {
-    type: ADD_WORD,
+    type: types.ADD_WORD,
     word: {
       id: uuid.v4(),
       text: word
