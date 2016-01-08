98b22be8da2c158d06782e21529ccee524379b15
web-padawan <skamandr@yandex.ru>
2016-01-08 18:10:03 +0200

    [mod] sort letters list

--- app/reducers/letters.js
+++ app/reducers/letters.js
@@ -5,7 +5,7 @@ const initialState = [];
 export default function letters(state = initialState, action) {
   switch (action.type) {
     case types.UPPD_LETTERS:
-      return (state.indexOf(action.letter) === -1) ? [...state, action.letter] : state;
+      return (state.indexOf(action.letter) === -1) ? [...state, action.letter].sort() : state;
 
     default:
       return state;
